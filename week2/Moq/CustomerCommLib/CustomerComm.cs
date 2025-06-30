7b59ec124c2db41cb1b9f9b5a8ec0ba792fc2e91
AyushmanPanigrahi07 <ayushmannn2004@gmail.com>
2025-06-30 10:57:56 +0530

    Adding week2 folder after cleanup

---
+++ week2/Moq/CustomerCommLib/CustomerComm.cs
@@ -0,0 +1,19 @@
+﻿namespace CustomerCommLib
+{
+    public class CustomerComm
+    {
+        private IMailSender _mailSender;
+
+        public CustomerComm(IMailSender mailSender)
+        {
+            _mailSender = mailSender;
+        }
+
+        public bool SendMailToCustomer()
+        {
+            
+            _mailSender.SendMail("cust123@abc.com", "Thanks for your purchase!");
+            return true;
+        }
+    }
+}
---
+++ week2/Nunit/CalcLibrary.Tests/CalculatorTests.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using CalcLibrary;
+
+namespace CalcLibrary.Tests
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        private Calculator _calculator;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _calculator = new Calculator();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _calculator = null;
+        }
+
+        [Test]
+        public void Add_TwoNumbers_ReturnsCorrectSum()
+        {
+            int result = _calculator.Add(4, 5);
+            Assert.That(result, Is.EqualTo(9));
+        }
+    }
+}
